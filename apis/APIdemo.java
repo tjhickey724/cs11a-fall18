03e92a11d407ce23e5f8e85aec25cb218573da8e
tim <tjhickey@brandeis.edu>
2018-11-07 18:35:40 -0500

    added compilation instructions to javadoc

--- apis/APIdemo.java
+++ apis/APIdemo.java
@@ -21,6 +21,11 @@ here's an example ...
 "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
 which returns a JSON string containing weather info like temperature and wind windspeed
 
+Compile this as
+% javac -classpath ".:gson-2.8.5.jar" APIdemo.java
+then run it as
+% java -classpath ".:gson-2.8.5.jar" APIdemo
+
 */
 
 
