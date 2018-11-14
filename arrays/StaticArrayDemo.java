4c2211ff326999a8b085787823ae182501c3bc9c
Timothy J Hickey <tjhickey@brandeis.edu>
2018-11-14 13:12:44 -0500

    broke for loop into two lines

--- arrays/StaticArrayDemo.java
+++ arrays/StaticArrayDemo.java
@@ -1,6 +1,7 @@
 public class StaticArrayDemo {
-  public static final int SIZE=3;
+  public static final int SIZE=5;
   public static int[] data = new int[SIZE];
+
   public static void main(String[] args){
     initData(); // notice we aren't passing a parameter or receiving a values
     printData();
@@ -8,8 +9,10 @@ public class StaticArrayDemo {
   public static void initData(){
     data[0]=1;
     data[SIZE-1]=1;
-    for(int i=1; i<SIZE-1; i++){ data[i] = data[i-1]*(SIZE-i)/i;}
+    for(int i=1; i<SIZE-1; i++){
+      data[i] = data[i-1]*(SIZE-i)/i;}
   }
+
   public static void printData(){
     System.out.printf("%s%n",java.util.Arrays.toString(data));
   }
